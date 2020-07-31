edb7df9a0f75099eeb0136ebd5293ff89832fe20
Olusola Oseni <kenolusola@gmail.com>
2020-07-31 19:27:19 +0100

    Add schema, controller and routes for courses

---
+++ controller/courses.js
@@ -0,0 +1,26 @@
+const Course = require('../models/Course')
+const asyncHandler = require('../middleware/asyncHandler')
+const ErrorResponse = require('../utils/errorResponse')
+
+
+// @desc			Get all courses
+// @route			GET  /api/v1/courses
+// @route			GET  /api/v1/bootcamps/:bootcampId/courses
+// @access		Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query
+  const { bootcampId }  = req.params
+  if (bootcampId) {
+    query = Course.find( { bootcamp: bootcampId })
+  } else {
+    query = Course.find()
+  }
+
+  const courses = await query
+
+  res.status(200).json({
+    status: 'success',
+    count: courses.length,
+    data: courses
+  })
+})
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 
 const {
   getBootcamps,
@@ -9,6 +8,14 @@ const {
   deleteBootcamp,
   getBootcampsInRadius
 } = require('../controller/bootcamps');
+
+// Include other resource router
+const courseRouter = require('./courses')
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
 
 router
   .route('/radius/:zipcode/:distance')
