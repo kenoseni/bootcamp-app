0b7761745d1e6e84ad945d868b1fca1ac9a370c1
Olusola Oseni <kenolusola@gmail.com>
2020-08-11 15:37:37 +0100

    Add course and user relationship and allow owner of course and admin to add, update and delete course

--- controller/courses.js
+++ controller/courses.js
@@ -49,11 +49,19 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 exports.createCourse = asyncHandler(async (req, res, next) => {
   const { bootcampId } = req.params
   req.body.bootcamp = bootcampId
+  req.body.user = req.user.id
 
   const bootcamp = await Bootcamp.findById(bootcampId)
 
   if (!bootcamp) {
     return next(new ErrorResponse(`Resource not found with id of ${id}`, 404))
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(
+      `User ${req.user.id} is not authorized to add a course to ${bootcamp._id}`, 401
+    ))
   }
 
   const course = await Course.create(req.body)
@@ -77,6 +85,13 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Resource not found with id of ${id}`, 404))
   }
 
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(
+      `User ${req.user.id} is not authorized to update course ${course._id}`, 401
+    ))
+  }
+
   course = await Course.findByIdAndUpdate(id, req.body, {
     new: true,
     runValidators: true
@@ -98,6 +113,13 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(new ErrorResponse(`Resource not found with id of ${id}`, 404))
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(
+      `User ${req.user.id} is not authorized to delete course ${course._id}`, 401
+    ))
   }
 
   await course.remove()
